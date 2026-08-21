429df7c7e45f4cc8f4b4cb8eb2be12871c2ac454
stefan-hacks <stefanmrobertson@gmail.com>
2026-08-21 15:21:14 -0300

    updated

--- flake.nix
+++ flake.nix
@@ -5,34 +5,38 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs =
+    { self, nixpkgs, ... }:
     let
       supportedSystems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-      nixpkgsFor = forAllSystems (system: import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      });
-     in
-     {
-       packages = forAllSystems (system: {
-         hello = nixpkgsFor.${system}.hello;
+      nixpkgsFor = forAllSystems (
+        system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        }
+      );
+    in
+    {
+      packages = forAllSystems (system: {
+        hello = nixpkgsFor.${system}.hello;
       });
 
       devShells = forAllSystems (system: {
-        default = nixppkgsFor.${system}.mkShell { 
+        default = nixppkgsFor.${system}.mkShell {
           packages = with nixpkgsFor.${system}; [
             hello
             git
             vim
           ];
- m        shellHook = ''
+          shellHook = ''
             echo  "Welcome to nix!"
             echo "This shell is defined in flake.nix"
           '';
         };
-       });
+      });
 
       formatter = forAllSystems (systems: nixpkgsFor.${system}.nixpkgs-fmt);
     };
